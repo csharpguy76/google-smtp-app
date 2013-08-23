0e47a75d5041d4f08328c3e495d64dd96b08f00b
csharpguy76 <csharpguy76@gmail.com>
2013-08-23 23:51:39 +0800

    Created

--- google-smtp-app/Program.cs
+++ google-smtp-app/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,25 @@ namespace google_smtp_app
     {
         static void Main(string[] args)
         {
+            // from and recipient can be replaced with your own email addresses.
+            // for the network credentials you will need to replace it with your own
+            // gmail email address and password.
+
+            const string from = "username@gmail.com";
+            const string recipient = "receipient@gmail.com";
+            const string subject = "Smtp Test";
+            const string body = "This email was sent using Google's Smtp server!";
+
+            var smptClient = new SmtpClient("smtp.gmail.com", 587)
+            {
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential("username@gmail.com", "password"),
+                EnableSsl = true
+            };
+            
+            smptClient.Send(from, recipient, subject, body);
+
         }
     }
 }
